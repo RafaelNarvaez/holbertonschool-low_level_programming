7e866d19fc0964172005e6a5cd805213d1f28fed
Rafael <r-enc@hotmail.com>
2021-10-02 03:58:31 +0000

    Add: Task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - function that prints numbers
+ *Return: 0
+ */
+
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
