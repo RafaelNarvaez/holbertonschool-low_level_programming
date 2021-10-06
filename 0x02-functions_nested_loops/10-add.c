3812265676b9bf94471888281e406b1fdec2c50f
Rafael <r-enc@hotmail.com>
2021-10-06 13:42:14 +0000

    Add: New Task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *add - function that adds 2 numbers
+ *@idx: variable
+ *@idy: variable
+ *Return: the sum of the numbers *
+ */
+
+int add(int idx, int idy)
+{
+	return (idx + idy);
+}
