022b83b1800a65c10b6687ad71ba5d6493be27b7
Rafael <r-enc@hotmail.com>
2021-10-06 01:00:12 +0000

    New Task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *main - function that prints character 1 by 1
+ * follow by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	reutn (0);
+}
