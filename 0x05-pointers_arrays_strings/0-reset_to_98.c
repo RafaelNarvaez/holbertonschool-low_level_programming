fd77672c3b0815ca2335c72f69242e44f5cfa99b
Rafael <r-enc@hotmail.com>
2021-10-11 20:03:03 +0000

    Add: New Task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that pointer to an integer
+ *@n: is the given parameter (variable)
+ *@*n: the pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
