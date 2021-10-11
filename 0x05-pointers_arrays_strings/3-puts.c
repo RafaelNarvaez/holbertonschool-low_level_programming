64e4068ee32e63a88afef4b75f61b7925959e51d
Rafael <r-enc@hotmail.com>
2021-10-11 20:50:59 +0000

    Add: New Task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts - function that prints a string
+ *@str: given varaible
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
