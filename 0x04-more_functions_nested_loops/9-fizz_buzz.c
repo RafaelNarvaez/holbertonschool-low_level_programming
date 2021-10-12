8dc6e848717a905268992b9cad5d6a5c3522c55d
Rafael <r-enc@hotmail.com>
2021-10-12 02:10:13 +0000

    New Task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - function (Fizz Buzz)
+ *Return: 0
+ */
+
+int main(void)
+{
+	int idx;
+
+		for (idx = 1; idx <= 99; idx++)
+		{
+			if (idx %3 == 0 && idx %5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else if (idx %3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else if (idx %5 == 0)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("%d ", idx);
+			}
+		}
+		printf("Buzz");
+		putchar('\n');
+		return (0);
+}
