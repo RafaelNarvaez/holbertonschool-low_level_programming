a51f892f17b800395fe05e1d9e0649d1607b8630
Rafael <r-enc@hotmail.com>
2021-10-12 00:32:41 +0000

    Add: New Task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_numbers - function that print numbers
+ */
+
+void print_numbers(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
