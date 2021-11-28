bf0e96bc40abccc9072dfa80a20a9ad9b9f21c76
Rafael <r-enc@hotmail.com>
2021-11-28 17:41:21 +0000

    Add:New File

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,24 +10,28 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int idx, idy, dist;
+	int idx, idy;
+	int dist = 0;
 
-	for (dist = 0; needle[dist] != '\0'; dist++)
+	while (needle[dist] != '\0')
+	{
+		dist++;
+	}
 
-		for (idx = 0; haystack[idx] != '\0'; idx++)
+	for (idx = 0; haystack[idx] != '\0'; idx++)
+	{
+		for (idy = 0; needle[idy] != '\0'; idy++)
 		{
-			for (idy = 0; needle[idy] != '\0'; idy++)
+			if (haystack[idx + idy] != needle[idy])
 			{
-				if (haystack[idx + idy] != needle[idy])
-				{
-					break;
-				}
+				break;
 			}
-			if (idx == dist)
-			{
-				return (&haystack[idx]);
-			}
-
 		}
+		if (idx == dist)
+		{
+			return (&haystack[idx]);
+		}
+
+	}
 	return (NULL);
 }
