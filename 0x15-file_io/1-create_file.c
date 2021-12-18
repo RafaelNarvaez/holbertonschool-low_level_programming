b9ca6ff921b60615177900f623a242ac8b412624
Rafael <r-enc@hotmail.com>
2021-12-18 16:50:22 +0000

    Add:Update File

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,15 +1,10 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "main.h"
 
 /**
  * create_file - creates a new file and copies text into it
  * @filename: the name of file to create
  * @text_content: text to copy into new file
- * Return: 1 on success, -1 on failure (file can not be created, or written,
- * or write fails, etc).
+ * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
@@ -17,17 +12,30 @@ int create_file(const char *filename, char *text_content)
 	int fd, err, len;
 
 	if (filename == NULL)
-	return (-1);
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
+
+		close(fd);
+		return (1);
+	}
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fd < 0)
+	if (fd == -1)
 		return (-1);
 
-	while (text_content && *(text_content + len))
+	while (text_content[len] != '\0')
+	{
 		len++;
+	}
 	err = write(fd, text_content, len);
-	close(fd);
-	if (err < 0)
+	if (err == -1)
 		return (-1);
+
+	close(fd);
 	return (1);
 }
