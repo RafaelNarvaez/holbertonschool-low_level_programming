17c594a75b1d6b0371374ab2abcd40ddc7ec09ae
Rafael <r-enc@hotmail.com>
2021-12-18 17:16:13 +0000

    Add:Update File

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,23 +14,28 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, err, len;
+	int fd, err, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
+
 	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd < 0) /* failed opening file */
 		return (-1);
-	if (!text_content)
+	if (text_content == NULL)
 	{
 		close(fd);
 		return (1);
 	}
-	while (*(text_content + len))
+	while (text_content[len])
+	{
 		len++;
+	}
 	err = write(fd, text_content, len);
-	close(fd);
-	if (err < 0)
+
+	if (err == -1)
 		return (-1);
+
+	close(fd);
 	return (1);
 }
