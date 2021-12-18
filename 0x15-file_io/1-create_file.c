4fc9ff60679839a3fab5b6613dc9d896376e68da
Rafael <r-enc@hotmail.com>
2021-12-18 16:37:33 +0000

    Add:Update File

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,8 +16,9 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, err, len;
 
-	if (!filename)
-		return (-1);
+	if (filename == NULL)
+	return (-1);
+
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
