3c86602a7594878579b4d4b99247f350391e8773
Rafael <r-enc@hotmail.com>
2022-03-18 01:48:05 +0000

    Add:New Task

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Island Perimeter Function
+"""
+
+
+def island_perimeter(grid):
+    """ Calculate a perimeter of an island (grid)
+        Grid: the list representation of the island
+        Return: perimeter
+    """
+
+    if grid is None:
+        return None
+
+    h = 0
+    w = 0
+    for row in grid:
+        try:
+            if (row.index(1) >= 0):
+                h = h + 1
+        except ValueError:
+            pass
+        for row in grid:
+            w = max(row.count(1), w)
+
+            return (h * 2) + (w * 2)
