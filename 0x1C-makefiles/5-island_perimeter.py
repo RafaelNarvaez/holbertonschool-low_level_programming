f386ced03e820e83313dd385d1adebbeeb796303
Rafael <r-enc@hotmail.com>
2022-03-24 03:51:03 +0000

    Add:Task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-""" Island Perimeter Function
-"""
+"""island_ perimeter module."""
 
 
 def island_perimeter(grid):
-    """ Calculate a perimeter of an island (grid)
-        Grid: the list representation of the island
-        Return: perimeter
-    """
-
-    if grid is None:
-        return None
-    h = 0
-    w = 0
-    for row in grid:
-        try:
-            if (row.index(1) >= 0):
-                h = h + 1
-        except ValueError:
-            pass
-    for row in grid:
-        w = max(row.count(1), w)
-
-    return (h * 2) + (w * 2)
+    """Function that calculates the perimeter of an island on a grid."""
+    i = 0
+    j = 0
+    p = 0
+    if grid is None or type(grid) is not list or type(grid[0]) is not list:
+        return 0
+    l = len(grid)
+    l2 = len(grid[0])
+    while i < l:
+        while j < l2:
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    p += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    p += 1
+                if j == l2 - 1 or grid[i][j + 1] == 0:
+                    p += 1
+                if i == l - 1 or grid[i + 1][j] == 0:
+                    p += 1
+            j += 1
+        j = 0
+        i += 1
+    return p
